e99e5cb4d61f2c3c003d5c8bcac168f37482b499
apple <apple@MacBook-Pro.local>
2023-01-15 17:48:01 +0100

    initial commit

---
+++ ContextMenuDemo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ContextMenuDemo
+//
+//  Created by apple on 15/1/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ContextMenuDemo/ContextMenuDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContextMenuDemoApp.swift
+//  ContextMenuDemo
+//
+//  Created by apple on 15/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct ContextMenuDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
